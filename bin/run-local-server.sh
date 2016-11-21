5ec204614dbb76037d1b8623ba3a1d503f3643c7
Benton Roberts <broberts@mdsol.com>
2016-11-21 14:36:32 -0800

    refine setup scripts

--- bin/run-local-server.sh
+++ bin/run-local-server.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
-# runs the Spigot server on localhost port 25565
-# data is stored in ./data
+# Users docker to run a server on localhost port 25565
+# data is stored in k8craft/data/
 set -e
 
+abs_path() { # Returns the absolute path of this script regardless of symlinks
+    SOURCE="${BASH_SOURCE[0]}"
+    while [ -h "$SOURCE" ]; do
+        DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+        SOURCE="$(readlink "$SOURCE")"
+        [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+    done
+    echo "$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+} # From: http://stackoverflow.com/a/246128
+K8C_HOME=$(cd `abs_path`/.. && pwd)
+
+echo "Running local minecraft server with data in ${K8C_HOME}/data/..."
 docker-compose up spigot
