135749c5bc26863c565474381b54b77e7f8896c3
Benton Roberts <broberts@mdsol.com>
2016-11-22 11:51:51 -0800

    add link to data dir for SSH user

--- src/ssh/add-ssh-user.sh
+++ src/ssh/add-ssh-user.sh
@@ -11,7 +11,10 @@ if ! $(id $SSH_USER >/dev/null 2>&1) ; then
   echo "Creating user ${SSH_USER}..."
   useradd $SSH_USER -m --gid $SSH_GID --uid $SSH_UID -s /bin/bash
 fi
+
+# add hooks to data dir
 echo "cd /data" >>/home/${SSH_USER}/.profile
+ln -sf /data /home/${SSH_USER}/data
 
 echo "Installing SSH key ${SSH_KEY}..."
 KEYFILE="/home/${SSH_USER}/.ssh/authorized_keys"
